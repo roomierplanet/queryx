7c5ae933d10e2667f5fcec76faae8f9691668e96
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2023-05-17 16:30:46 -0400

    Added primereact import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.scss';
+import 'primereact/resources/themes/bootstrap4-dark-purple/theme.css'
+import "primereact/resources/primereact.min.css"
+import 'primeicons/primeicons.css';
+        
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
