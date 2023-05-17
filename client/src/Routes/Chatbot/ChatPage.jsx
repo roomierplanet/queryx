d2575aecb521eee376a2e31e989512c317d6c092
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2023-05-17 16:31:11 -0400

    Created chat page

--- client/src/Routes/Chatbot/ChatPage.jsx
+++ client/src/Routes/Chatbot/ChatPage.jsx
@@ -1,7 +1,14 @@
 import React, {useEffect, useState} from 'react'
+import { InputTextarea } from 'primereact/inputtextarea';
+        
+        
 
 export default function ChatPage() {
     const [loaded, setLoaded] = useState(false)
+    const [sessionStart, setSessionStart] = useState(false)
+    const [query, setQuery] = useState("")
+    const [chatHistory, setChatHistory] = useState([])
+    const [loadingResponse, setLoadingResponse] = useState(false)
     useEffect(() => {
         const updateDB = async () => {
             await fetch('http://127.0.0.1:8000/api/v1/update/')
@@ -11,21 +18,88 @@ export default function ChatPage() {
         if (loaded) {
             setLoaded(true)
         } else {
-            console.log("Calling the API")
             localStorage.setItem("loaded", "true")
             // call the API, wait for it
             updateDB()
-
         }
 
         return () => {
             
         }
     }, []);
+    const getQuery = async (query) => {
+        const queryBody = {
+            queryText: query
+        }
+        const response = await fetch('http://127.0.0.1:8000/api/v1/query/', {
+            method: 'POST', 
+            body: JSON.stringify(queryBody)
+        })
+        const jsonResponse = await response.json()
+        return jsonResponse.Result
+    }
+    const handleQuerySubmit = async () => {
+        if (query === "") return     
+        const tempQuery = query
+        // reset the input field
+        setQuery("")   
+        // if the session not started, indicate start of session
+        if (!sessionStart) {
+            setSessionStart(true)
+            document.querySelector(".loaded-page").className = "loaded-page-queried"
+        }
+        setChatHistory(hist => [...hist, tempQuery])
+        // indicate initiation of query retrieval
+        setLoadingResponse(true)
+        // call api with query and get response
+        const queryResponse = await getQuery(tempQuery)
+        setChatHistory(hist => [...hist, queryResponse])
+        setLoadingResponse(false)
+    }
     return (
         <div className="chatPage">
             {loaded ?
-                <h1 style={{color: 'white'}}>Loaded</h1> :
+                <div className="loaded-page">
+                    {!sessionStart &&
+                    <div className="temp-text">
+                        <h1 style={{color: 'white'}}>Ask me anything</h1>
+                        <p>Your wish is my command!</p>
+                    </div>
+                    }
+                    {sessionStart &&
+                        <div className="chat-area">
+                            {chatHistory.map((msg, i) => {
+                                return (
+                                    <div className="chat-message" id={i % 2 === 1 ? 'user' : 'gpt'}>
+                                        <p>{msg}</p>
+                                    </div>
+                                ) 
+                                })}
+                        </div>
+                    }
+                    {loadingResponse &&
+                    <div className="gen-text">
+                        <i  className="pi pi-spin pi-cog" 
+                            style={{ fontSize: '2rem', color: 'rgb(94, 0, 246)', margin: '0' }}></i>
+                        <div className="loading">
+                        <p style={{marginTop: '0.5rem'}}>Generating...</p>
+                        </div>
+                    </div>       
+                    }
+                    <InputTextarea
+                        placeholder="Send a message" 
+                        autoResize
+                        value={query} 
+                        onChange={(e) => setQuery(e.target.value)}
+                        onKeyDown={(e) => {if (e.code === "Enter" && !e.shiftKey) {
+                            e.preventDefault()
+                            handleQuerySubmit()
+                        } }}
+                        rows={1} style={{width: '50%'}} />
+                </div>
+
+                :
+
                 <div className="loading">
                 <div className="lds-ripple">
                     <div>
